6f559f513800d0d33f3fe9f288bea319c6f850ba
Ebuka-Chucks <chrisbrandford4@gmail.com>
2024-08-23 22:12:10 +0100

    change made

--- server/src/helpers/dbconfig.js
+++ server/src/helpers/dbconfig.js
@@ -4,15 +4,13 @@ const { DB_URI } = require("../config/index");
 
 //Database Connection configuration
 const dbconnect = async () => {
-  await mongoose
-    .connect(DB_URI)
-    .then(() => {
+  await mongoose;
+  mongoose
+    .connect(DB_URI, { serverSelectionTimeoutMS: 30000 }).then(() => {
       console.log("Database Connected successfully");
     })
     .catch((error) => {
       console.log(error);
     });
-  
 };
 module.exports = dbconnect;
-
