0de53f838c66ebd8374bd1480c6b28e468cc0d86
Ebuka-Chucks <chrisbrandford4@gmail.com>
2024-09-03 14:56:11 +0100

    chore: Update cors origin to include 'https://goldenairwaycourier-website.vercel.app/'

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   });
 
 //global middleware configuration for cors
-app.use('*', cors({
+app.use( cors({
     origin: ['https://goldenairwayadminpanel.vercel.app', 'https://goldenairwaycourier-website.vercel.app/'], // Your actual frontend domains
     credentials: true // Allows cookies and other credentials to be sent
   }));
